a2835bee29c29f8fd9a6264eda7e0cd232047dbb
Scott Carter <scarter28m-github@yahoo.com>
2018-01-01 14:07:21 -0500

    1.0.2  Renamed framework.

---
+++ AppDeveloperKit_Bonjour.framework/Headers/AppDeveloperKit_Bonjour.h
@@ -0,0 +1,21 @@
+//
+//  AppDeveloperKit_Bonjour.h
+//  AppDeveloperKit_Bonjour
+//
+//  Created by Scott Carter on 6/22/17.
+//  Copyright © 2017 Scott Carter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppDeveloperKit_Bonjour.
+FOUNDATION_EXPORT double AppDeveloperKit_BonjourVersionNumber;
+
+//! Project version string for AppDeveloperKit_Bonjour.
+FOUNDATION_EXPORT const unsigned char AppDeveloperKit_BonjourVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppDeveloperKit_Bonjour/PublicHeader.h>
+
+
+#import <AppDeveloperKit_Bonjour/FS_Bonjour.h>
+
